228de82aa6bcbe64a7f19ddfee70987e0e041dda
Koustubh-lapate <koustubh.lapate@gmail.com>
2024-01-10 14:39:45 +0530

    modified:   index.js 	modified:   index.ts

--- index.js
+++ index.js
@@ -3,3 +3,40 @@ function Greet() {
 }
 var ans = Greet();
 console.log(ans);
+var sales = 12;
+var course = 'TypeScript';
+var is_published = true;
+console.log(sales + ' ' + course + ' ' + is_published);
+function calculator(num1, num2, operation) {
+    var ans = 0;
+    if (operation === '+') {
+        ans = num1 + num2;
+    }
+    else if (operation === '-') {
+        ans = num1 - num2;
+    }
+    else if (operation === '*') {
+        ans = num1 * num2;
+    }
+    else if (operation === '/') {
+        ans = num1 / num2;
+    }
+    return ans;
+}
+console.log(calculator(1, 2, '*'));
+console.log(calculator(2, 4, '+'));
+console.log(calculator(4, 2, '/'));
+console.log(calculator(9, 3, '-'));
+function vote(age) {
+    if (age >= 18) {
+        return true;
+    }
+    return false;
+}
+var myAge = vote(19);
+if (myAge === true) {
+    console.log('You are eligible for voting.');
+}
+else {
+    console.log('You are not eligible for voting.');
+}
--- index.ts
+++ index.ts
@@ -5,3 +5,54 @@ function Greet(): string {
 let ans: string = Greet();
 
 console.log(ans);
+
+let sales: number = 12;
+let course: string = 'TypeScript';
+let is_published: boolean = true;
+
+console.log(sales + ' ' + course + ' ' + is_published);
+
+function calculator(num1: number, num2: number, operation: string): number{
+    let ans: number = 0;
+    
+    if(operation === '+'){
+        ans = num1 + num2;
+    }
+
+    else if(operation === '-'){
+        ans = num1 - num2;
+    }
+
+    else if(operation === '*'){
+        ans = num1 * num2;
+    }
+
+    else if(operation === '/'){
+        ans = num1 / num2;
+    }
+    
+    return ans;
+}
+
+console.log(calculator(1, 2, '*'));
+console.log(calculator(2, 4, '+'));
+console.log(calculator(4, 2, '/'));
+console.log(calculator(9, 3, '-'));
+
+function vote(age: number): boolean{
+    if(age >= 18){
+        return true;
+    }
+
+    return false;
+}
+
+let myAge: boolean = vote(19);
+
+if(myAge === true){
+    console.log('You are eligible for voting.');
+}
+
+else{
+    console.log('You are not eligible for voting.');
+}
